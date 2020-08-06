ff6ef2fbafb605776aebe4d2ff891134175ce60c
shibli2700 <mohdshibli27@gmail.com>
2020-08-06 11:04:06 +0530

    palindrome program

---
+++ vars.py
@@ -0,0 +1,24 @@
+#!/usr/share/python3
+
+#int c = 23;
+
+#variable_name=<value>
+
+#palidrome string
+
+str1=input("Enter a string: ") #by default the data type will be a str<>
+if (str1 == str1[::-1]):
+    print("Palindrome")
+else:
+    print("Not a palindrome")
+
+
+
+#list = [2, 3, 4, 5] -> can be modified, read/write
+#tuple = (2, 3, 4, 5) -> read-only
+#dictionary = {<key>:<value>, <key>:<value>} -> read/write
+
+
+
+
+
